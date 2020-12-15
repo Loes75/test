7e64eae65099151da19585f03c9652be0060d255
Laura Ochoa <laura.ochoa@imagineapps.co>
2020-12-14 21:53:33 -0500

    cambio es regex validator para aceptar también tildes

--- project/usuarios/models.py
+++ project/usuarios/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 class Usuario(models.Model):
-    nombre=models.CharField(max_length=30,blank=False,validators=[RegexValidator(r'^[a-zA-z]+$',('Ingresa unicamente letras'))])
-    apellido=models.CharField(max_length=40,blank=False,validators=[RegexValidator(r'^[a-zA-z]+$',('Ingresa unicamente letras'))])
-    ciudad=models.CharField(max_length=40,blank=False,validators=[RegexValidator(r'^[a-zA-z]+$',('Ingresa unicamente letras'))])
+    nombre=models.CharField(max_length=30,blank=False,validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$',('Ingresa unicamente letras'))])
+    apellido=models.CharField(max_length=40,blank=False,validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$',('Ingresa unicamente letras'))])
+    ciudad=models.CharField(max_length=40,blank=False,validators=[RegexValidator(r'^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$',('Ingresa unicamente letras'))])
     direccion=models.CharField(max_length=100,blank=False)
     estadogeo=models.JSONField(blank=True,null=True)
     longitud=models.FloatField(blank=True,null=True)
